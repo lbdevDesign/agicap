90230afd2390f93b2c4771fe62647b4fb6070f27
Lucas <lucasbombaron@gmail.com>
2024-06-13 14:22:22 +0200

    presentation Data

---
+++ src/data/presentation.js
@@ -0,0 +1,44 @@
+const presentationData = [
+    {
+        id: 1,
+        img: "FR___Consolidation.svg",
+        alt: "Consolidation",
+        title: "Combinez vos données pour des prévisions à tous les niveaux (groupe, divisions, filiales...)",
+        list: [
+            {
+                id: 1,
+                text: "Unifiez les données prévisionnelles relatives aux opérations, au financement et aux investissements dans une interface unique"
+            },
+            {
+                id: 2,
+                text: "Convertissez vos comptes de résultats prévisionnels en prévisions de trésorerie grâce à notre convertisseur personnalisé"
+            },
+            {
+                id: 3,
+                text: "Connectez Excel à Agicap pour importer votre budget ou toute autre donnée utile aux projections de trésorerie"
+            },
+        ] 
+    },
+    // {
+    //     id: 2,
+    //     img: "FR___Scénarios.svg",
+    //     alt: "Consolidation",
+    //     title: "Combinez vos données pour des prévisions à tous les niveaux (groupe, divisions, filiales...)",
+    //     list: [
+    //         {
+    //             id: 1,
+    //             text: "Unifiez les données prévisionnelles relatives aux opérations, au financement et aux investissements dans une interface unique"
+    //         },
+    //         {
+    //             id: 2,
+    //             text: "Convertissez vos comptes de résultats prévisionnels en prévisions de trésorerie grâce à notre convertisseur personnalisé"
+    //         },
+    //         {
+    //             id: 3,
+    //             text: "Connectez Excel à Agicap pour importer votre budget ou toute autre donnée utile aux projections de trésorerie"
+    //         },
+    //     ] 
+    // }
+];
+
+export default presentationData;
