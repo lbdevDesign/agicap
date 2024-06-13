9a647c09cc76c3243feb8382f0ba2db240af5861
Lucas <lucasbombaron@gmail.com>
2024-06-13 13:22:33 +0200

    app & style update

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 //Organisms
 import Header from "../components/organisms/header/header";
 import HeroSection from "../components/organisms/heroSection/heroSection";
+import Features from "../components/organisms/features/features";
 
 function App() {
   return (
-    <div className="App">
+    <div className="appContainer">
       <Header />
       <HeroSection />
+      <Features />
     </div>
   );
 }
