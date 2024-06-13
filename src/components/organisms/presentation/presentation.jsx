6035a165f6fe0aa587ac5c7aa8c8460aa4092de9
Lucas <lucasbombaron@gmail.com>
2024-06-13 14:21:41 +0200

    Organism Presentation

---
+++ src/components/organisms/presentation/presentation.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+//Data
+import presentationData from "../../../data/presentation";
+
+//Molecules
+import PresentationModule from "../../molecules/presentationModule/presentationModule";
+
+function Presentation() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        setData(presentationData)
+      }, [])
+
+    return (
+        <div className="presentation">
+            <h2 className="presentation__title">Combinez vos données pour réaliser des prévisions de trésorerie et des scénarios fiables</h2>
+            {data.map((item) => (
+                <PresentationModule 
+                    key={item.id}
+                    img={item.img}
+                    title={item.title}
+                    list={item.list}
+                    alt={item.alt}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default Presentation;
