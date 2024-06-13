2356d1e6b529ece98e79520986c8f0765b783cec
Lucas <lucasbombaron@gmail.com>
2024-06-13 13:23:24 +0200

    data features

---
+++ src/data/features.js
@@ -0,0 +1,22 @@
+const featuresData = [
+    {
+        id:1,
+        img: "refresh-ccw-02_1.svg",
+        title: "Automatisez",
+        text: "la création d'un rolling forecast depuis différentes sources"
+    },
+    {
+        id:2,
+        img: "file-shield-02.svg",
+        title: "Réduisez les risques",
+        text: "face aux ralentissements économiques et la volatilité des marchés"
+    },
+    {
+        id:3,
+        img: "coins-hand_1.svg",
+        title: "Identifiez les opportunités",
+        text: "en visualisant clairement la trésorerie de votre entreprise"
+    }
+];
+
+export default featuresData;
